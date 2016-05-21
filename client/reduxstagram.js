d76b038a7d3efcc0b2d0d51ef9cb2b145a770a28
WajidKagzi <wjkagzi@gmail.com>
2016-05-20 23:39:11 -0400

    Adding sample react component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import CSS
+import css from './styles/style.styl';
+
+render(<p>hello world</p>, document.getElementById('root'));
