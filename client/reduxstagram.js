e3228be24447577543dc325db6e80138dc8d3d4b
WajidKagzi <wjkagzi@gmail.com>
2016-05-23 23:56:38 -0400

    Rendering main component in index page

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
-// import CSS
+// Import CSS
 import css from './styles/style.styl';
 
+//  Import Components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
