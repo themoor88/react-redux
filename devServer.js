6472701aea1f52b929141f6c5e2946ee8143636c
WajidKagzi <wjkagzi@gmail.com>
2016-06-01 23:29:05 -0400

    Change everything to ES6 syntax

--- devServer.js
+++ devServer.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import config from './webpack.config.dev';
 
-var app = express();
-var compiler = webpack(config);
+let app = express();
+let compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
